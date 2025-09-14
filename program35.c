1ee3a0fa0424f3e3cbb6ab3d2e8857291f4917e9
Chinmay2809 <chinmayjaiswal28@gmail.com>
2025-09-14 16:13:14 +0530

    a

---
+++ program35.c
@@ -0,0 +1,28 @@
+// Write a C program to print all factors of a given number.
+
+#include <stdio.h> 
+
+int main() { 
+    int num; 
+    int i;   
+
+    
+    printf("Enter a positive integer: "); 
+    
+    scanf("%d", &num); 
+
+    
+    printf("Factors of %d are: ", num);
+
+   
+    for (i = 1; i <= num; ++i) { 
+       
+        if (num % i == 0) { 
+            printf("%d ", i); 
+        }
+    }
+
+    printf("\n"); 
+
+    return 0; 
+}
