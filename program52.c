654e3c1170183628cb03d96ec47ab61eee419e6b
Chinmay2809 <chinmayjaiswal28@gmail.com>
2025-09-19 20:52:40 +0530

    a

---
+++ program52.c
@@ -0,0 +1,55 @@
+// Write a C program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+#include <stdio.h>
+
+int main() {
+   
+    printf("*\n\n");       
+
+    printf("*\n");         
+    printf("*\n\n");
+
+    printf("*\n");       
+    printf("*\n");
+    printf("*\n\n");
+
+    printf("*\n");         
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    printf("*\n");         
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    printf("*\n");         
+    printf("*\n");
+    printf("*\n\n");
+
+    printf("*\n");         
+
+    return 0;
+}
+
+
