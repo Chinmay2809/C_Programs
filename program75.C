b5b3a94607f824427d21574d892a1f75e4222b7d
chinmay28 <chinmayjaiswal28@gmail.com>
2025-10-02 11:39:48 +0530

    a

---
+++ program75.C
@@ -0,0 +1,48 @@
+// Write a C program to add two matrices.
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols; 
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix1[rows][cols];
+    int matrix2[rows][cols];
+    int sumMatrix[rows][cols];
+
+    printf("\nEnter elements for Matrix 1:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Enter element a[%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("\nEnter elements for Matrix 2:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Enter element b[%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("\nSum of the two matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", sumMatrix[i][j]); 
+        }
+        printf("\n"); 
+    }
+
+    return 0; 
+}
