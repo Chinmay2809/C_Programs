29e13d269a32725185c57578bde15088664b2508
Chinmay2809 <chinmayjaiswal28@gmail.com>
2025-10-07 19:11:31 +0530

    a

---
+++ program88.C
@@ -0,0 +1,26 @@
+// Write a C program to replace spaces with hyphens in a string.
+
+#include <stdio.h> 
+#include <string.h> 
+
+int main() {
+    char str[100]; 
+   
+
+    printf("Enter a string: "); 
+
+    fgets(str, sizeof(str), stdin); 
+
+    str[strcspn(str, "\n")] = 0; 
+
+    for (int i = 0; i < strlen(str); i++) {
+  
+        if (str[i] == ' ') {
+            str[i] = '-'; 
+        }
+    }
+
+    printf("Modified string: %s\n", str); 
+
+    return 0; 
+}
